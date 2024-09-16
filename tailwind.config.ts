a8920c5286c4bc87f7ecb4ea5510af67adc88fe5
Francisco Gutierrez <97089+freefrancisco@users.noreply.github.com>
2024-09-16 19:37:05 -0400

    add tailwind typography for prose

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,30 @@
 import { type Config } from "tailwindcss";
+import typography from "tailwindcss/typography";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  plugins: [
+    typography,
+  ],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: 'bg-gray-800', // Custom color for links in prose
+              fontWeight: "bold",
+              textDecoration: 'underline',
+            },
+            pre: {
+              backgroundColor: 'transparent', // Remove dark background
+              color: 'inherit', // Keep the same text color
+            },
+          },
+        },
+      },
+    },
+  },
 } satisfies Config;
