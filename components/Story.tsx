4a89f3567e99dec1843eaea65885acc33e1a3cfe
Francisco Gutierrez <97089+freefrancisco@users.noreply.github.com>
2024-09-16 14:21:53 -0400

    first draft of website

---
+++ components/Story.tsx
@@ -0,0 +1,69 @@
+import { Head } from "$fresh/runtime.ts";
+import { marked } from "https://deno.land/x/marked/mod.ts";
+// import { marked } from "https://deno.land/x/marked@v4.0.12/mod.ts"; // Ensure this version is correct
+
+
+const markdownContent = `
+In August 2023, I signed a contract with Alco Windows and Doors to install impact windows, a requirement from my HOA for insurance purposes. 
+Initially, Alco communicated frequently and efficiently, but that changed once they received full payment. 
+They arranged financing through a third party, and while I’ve been paying interest on the loan, Alco received the money upfront and has yet to deliver.
+
+Despite involving my HOA president and receiving a permit expiration warning from the city, Alco has made no progress. 
+The owner, Luis Alvarez, actively promotes his business online but hasn’t responded to my inquiries.
+
+I chose Alco based on their numerous positive reviews, but my experience has made me question their accuracy.
+ My HOA lawyers reviewed the contract, and I trusted their advice, but the year-long delay has left me paying for undelivered services, right as hurricane season begins.
+
+After the warning from the city, I sent an email to everyone I had been in contact with at Alco, including Luis Alvarez, 
+the founder and Managing Director, but received no response. 
+The Managing Director, Luis Alverez, is active online. 
+In his [LinkedIn profile](https://www.linkedin.com/in/luis-alvarez-0a7a7b57/), he claims expertise with SEO, 
+a strategy companies use to optimize search engine results, sometimes presenting a more favorable image than reality. 
+There is also a [Power100 Business Profile](https://power100.io/luis-alvarez-ceo-lander/) of Luis Alvarez where they emphasize his SEO expertise.
+
+![Luis Alvarez, Managing Director of Alco](https://media.licdn.com/dms/image/v2/D4D22AQEV2r4zV0s9NA/feedshare-shrink_800/feedshare-shrink_800/0/1701800543657?e=1729123200&v=beta&t=H78uLZZLG3BErTI-GbWMLRAxblVokjYJiSD23gis08Y)
+
+Luis Alvarez also maintains a profile on [Twitter (X)](https://x.com/la7483), and is listed on 
+the [Alco management team](https://www.alcoimpact.com/our-team/) page on their website, as well as having his own 
+company profile here: [Luis Alvarez Profile](https://www.alcoimpact.com/our-team/detail/?employee=luis-alvarez).
+
+Luis Alvarez was [honored by the Doral Chamber of Commerce](https://www.doralchamber.org/luis-alvarez-alco-windows-and-doors-honored/) in 2022. 
+In contrast to his public persona, Alco Windows and Doors has accumulated 44 complaints over the past three years, 
+with 31 filed in just the last year. Most complaints involve delays in installation, 
+unresponsive customer service, and failure to deliver products after full payment. 
+This can be found on the [Better Business Bureau Page For Complaints against Alco](https://www.bbb.org/us/fl/doral/profile/impact-windows/alco-windows-and-doors-llc-0633-90414965/complaints). 
+The situation is so critical that they have [lost their Better Business Bureau accreditation](https://www.bbb.org/us/fl/doral/profile/impact-windows/alco-windows-and-doors-llc-0633-90414965).
+
+On their website, they prominently display the positive Google reviews they have received, 
+but if you visit [their Google Maps Page](https://www.google.com/maps/place/Alco+Windows+and+Doors/@25.786464,-80.343327,16z/data=!4m14!1m7!3m6!1s0x88d9b8e4d8421cd1:0x65841e77fdc48647!2sAlco+Windows+and+Doors!8m2!3d25.786464!4d-80.3407467!16s%2Fg%2F11dxdkybdg!3m5!1s0x88d9b8e4d8421cd1:0x65841e77fdc48647!8m2!3d25.786464!4d-80.3407467!16s%2Fg%2F11dxdkybdg?entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3D), 
+you'll find many recent negative reviews, which are overshadowed by older positive reviews.
+
+There is also [A Report by NBC Miami](https://www.nbcmiami.com/responds/after-months-of-waiting-man-turns-to-nbc6-responds-for-help-with-home-improvement-project/3365036/) 
+exposing how Alco did something similar to another customer. I've embedded the full article at the end of this page for convenience.
+
+Alco is based in Doral according to their website's [Contact Us](https://www.alcoimpact.com/contact-us/) page.
+
+      1421 NW 89th Ct
+      Doral, FL 33172
+      (786) 550-2380
+      Contractor License #CGC1526312
+
+`;
+
+
+// custom renderer for mardkown links
+const renderer = new marked.Renderer();
+  renderer.link = (href, title, text) => {
+    return `<a href="${href}" class="text-blue-500 underline font-bold hover:text-blue-700">${text}</a>`;
+  };
+  
+
+
+export default  function Story(){
+  const htmlContent = marked(markdownContent, {renderer});
+  return <div class="text-lg space-y-6" dangerouslySetInnerHTML={{ __html: htmlContent }} />
+}  
+// Store your markdown content as a string
+ 
+
+
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,34 +1,66 @@
 import { Head } from "$fresh/runtime.ts";
+import Story from "../components/Story.tsx"
+
+const titleText = "Beware of Alco Windows and Doors | A Miami Doral Window and Door Company"
+
+const descriptionText = "Alco Windows and Doors got paid upfront over a year ago, and they still have not delivered the product. The Miami Beach city permit for the windows is expiring, I messaged the CEO Luis Alvarez directly after learning this but he has not responded. This could be a scam, beware of using them."
+const keywordsText = "Window Company,alco, windows, impact windows, glass, fraud, scam, miami, miami beach, doral, florida, southern florida, window installation, window replacement, Alco, Miami Beach, Miami, window replacement company, window replacement contractor, new windows, window installers"
+
+const HeadTag = () =>
+  <Head>
+    <title>{titleText}</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content={descriptionText}/>
+    <meta name="keywords" content={keywordsText}/>
+    <meta property="og:title" content={titleText} />
+    <meta itemprop="name" content={titleText} />
+    <meta property="og:description" content={descriptionText} />
+    <meta itemprop="description" content={descriptionText} />
+    <meta property="og:image" content="https://cmsplatform.blob.core.windows.net/wwwalcoimpactcom/logos/b0152ee5-d59a-4626-a6c8-cf9cd2f82a05.png" />
+    <meta itemprop="image" content="https://cmsplatform.blob.core.windows.net/wwwalcoimpactcom/logos/b0152ee5-d59a-4626-a6c8-cf9cd2f82a05.png" />
+    <meta property="og:url" content="https://www.alcoimpact.org/" />
+    <meta property="og:type" content="article" />
+    <meta property="twitter:card" content={titleText} />
+    <meta property="twitter:title" content={titleText} />
+    <meta property="twitter:description" content={descriptionText} />
+    <meta property="twitter:image" content="https://cmsplatform.blob.core.windows.net/wwwalcoimpactcom/logos/b0152ee5-d59a-4626-a6c8-cf9cd2f82a05.png" />
+    <link rel="canonical" href="https://www.alcoimpact.org/" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
+  </Head>
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Alco Fraud Exposé</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Exposing fraudulent activities by Alco, a hurricane window and glass door company." />
-        <meta name="keywords" content="alco, windows, impact windows, glass, fraud, scam, miami, miami beach" />
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
-      </Head>
-      <div class="min-h-screen bg-gray-100">
-        <header class="bg-gray-800 text-white py-4">
-          <h1 class="text-center text-4xl font-bold">My Experience with Alco</h1>
-        </header>
-        <main class="py-8 px-4">
-          <h2 class="text-2xl font-semibold mb-4">Alco Impact Is A Fraud</h2>
-          <p class="text-lg">
-            Do not buy impact windows from Alco. They have a lot of fake reviews
-            on Google, but the reality is very different.
-          </p>
-          <p>
-            More content coming here soon...
-          </p>
-        </main>
-        <footer class="bg-gray-800 text-white py-4 text-center">
-          <p>© 2024 Alco Fraud Exposé</p>
-        </footer>
-      </div>
-    </>
+<>
+<HeadTag/>
+<div class="min-h-screen bg-gray-100">
+<header class="bg-gray-800 text-white py-4">
+  <h1 class="text-center text-4xl font-bold">Beware of Alco Windows And Doors</h1>
+  <h2 class="text-center text-2xl">Owned by Luis Alvarez</h2>
+  <h2 class="text-center">They sell hurricane impact windows in Miami area, took payment in full over a year ago, they have not delivered, and they have stopped responding. There are multiple complaints against them in the BBB. </h2>
+</header>
+<main class="py-8 px-4">
+  <Story/>
+  <iframe
+    src="https://www.nbcmiami.com/responds/after-months-of-waiting-man-turns-to-nbc6-responds-for-help-with-home-improvement-project/3365036/"
+    class="
+      w-full
+      sm:w-11/12
+      md:w-3/4
+      h-[65vh]
+      sm:h-[75vh]
+      md:h-[85vh]
+    "
+    style="border:0;"
+    allowfullscreen
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"
+  ></iframe>
+</main>
+<footer class="bg-gray-800 text-white py-4 text-center">
+  <p>Beware of Alco Windows And Doors</p>
+</footer>
+</div>
+</>
   );
 }
